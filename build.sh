11a11bace37e6cbd0c7daac044b5986d2b5dd975
Denis Kaznacheev <denkaz77@gmail.com>
2025-06-26 20:24:27 +0300

    Fix deployment configuration

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Установка системных зависимостей для Pillow и других пакетов
+apt-get update -y
+apt-get install -y python3-dev python3-pip python3-setuptools python3-wheel
+apt-get install -y libtiff5-dev libjpeg8-dev libopenjp2-7-dev zlib1g-dev \
+    libfreetype6-dev liblcms2-dev libwebp-dev tcl8.6-dev tk8.6-dev python3-tk \
+    libharfbuzz-dev libfribidi-dev libxcb1-dev
+
+# Установка зависимостей Python
+pip install -r requirements.txt
+
+# Сборка фронтенда
+cd frontend
+npm install
+npm run build
+cd ..
+
+# Сбор статических файлов Django
+python manage.py collectstatic --noinput 
