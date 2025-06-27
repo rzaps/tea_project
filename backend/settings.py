0e88b6ee30a604c70b9639cab6af376e6355d238
Denis Kaznacheev <denkaz77@gmail.com>
2025-06-27 16:14:14 +0300

    fix: correct static files path in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'backend/static'),
 ]
 
 # Настройки WhiteNoise для правильной обработки статических файлов
